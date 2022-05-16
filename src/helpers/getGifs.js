6da5c6f6f4b1124cd6747c8538a4fd10b127e6f8
Eduardo Ornelas <raul.e.ornelas@gmail.com>
2022-05-16 17:24:50 -0500

    First Commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+export const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
+    category
+  )}&limit=10&api_key=dPYaAbpuR0ki5HfP9r4MhjJcXek90tvs`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+
+  const gifs = data.map((img) => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_medium.url,
+    };
+  });
+
+  return gifs;
+};
